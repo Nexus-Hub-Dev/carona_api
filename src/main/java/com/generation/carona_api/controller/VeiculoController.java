072fcf3f21306f528710b65702048e0feac6208a
Nayara <bastosnayara24@gmail.com>
2026-07-31 14:02:02 -0300

    :sparkles: CRUD da Entidade Veiculo

--- src/main/java/com/generation/carona_api/controller/VeiculoController.java
+++ src/main/java/com/generation/carona_api/controller/VeiculoController.java
@@ -1,5 +1,92 @@
 package com.generation.carona_api.controller;
 
-public class VeiculoController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.carona_api.model.Veiculo;
+import com.generation.carona_api.repository.VeiculoRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/veiculos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class VeiculoController {
+	@Autowired
+	private VeiculoRepository veiculoRepository;
+
+	@GetMapping
+
+	public ResponseEntity<List<Veiculo>> getAll() {
+
+		return ResponseEntity.ok(veiculoRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+
+	public ResponseEntity<Veiculo> getById(@PathVariable Long id) {
+
+		return veiculoRepository.findById(id)
+
+				.map(resposta -> ResponseEntity.ok(resposta))
+
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+	
+	@GetMapping("/modelo/{modelo}")
+	public ResponseEntity<List<Veiculo>> getAllByModelo(@PathVariable String modelo) {
+		return ResponseEntity.ok(veiculoRepository.findAllByModeloContainingIgnoreCase(modelo));
+	}
+	
+	@GetMapping("/placa/{placa}")
+	public ResponseEntity<Veiculo> getByPlaca(@PathVariable String placa) {
+
+	    return veiculoRepository.findByPlacaIgnoreCase(placa)
+	            .map(ResponseEntity::ok)
+	            .orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<Veiculo> post(@Valid @RequestBody Veiculo Veiculo) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(veiculoRepository.save(Veiculo));
+
+	}
+
+	@PutMapping
+	public ResponseEntity<Veiculo> put(@Valid @RequestBody Veiculo Veiculo) {
+
+		if (veiculoRepository.existsById(Veiculo.getId()))
+			return ResponseEntity.ok(veiculoRepository.save(Veiculo));
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+
+		Optional<Veiculo> Veiculo = veiculoRepository.findById(id);
+
+		if (Veiculo.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+		veiculoRepository.deleteById(id);
+	}
 }
--- src/main/java/com/generation/carona_api/model/Veiculo.java
+++ src/main/java/com/generation/carona_api/model/Veiculo.java
@@ -1,5 +1,107 @@
 package com.generation.carona_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_veiculos")
 public class Veiculo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo modelo é obrigatório!")
+	@Size(min = 5, max = 100, message = "O modelo deve ter no mínimo 5 e no máximo 100 caracteres")
+	@Column(length = 100)
+	private String modelo;
+
+	@Pattern(regexp = "^[A-Z]{3}-?[0-9]{4}$|^[A-Z]{3}-?[0-9][A-Z][0-9]{2}$", message = "Informe uma placa válida. Formatos aceitos: ABC1234 ou ABC1D34.")
+	@NotBlank(message = "O atributo placa é obrigatório!")
+	@Size(min = 7, max = 7, message = "O atributo placa deve conter exatamente 7 dígitos e não pode conter caracteres especiais!")
+	@Column(unique = true, length = 7)
+	private String placa;
+
+	@Size(max = 5000, message = "o atributo foto deve conter no máximo 5000 caracteres")
+	@Column(length = 5000)
+	private String foto;
+
+	@NotBlank(message = "O atributo cor é obrigatório!")
+	@Size(min = 3, max = 100, message = "O atributo cor deve ter no mínimo 3 e no máximo 100 caracteres")
+	@Column(length = 100)
+	private String cor;
+
+	@NotNull(message = "O atributo capacidade é obrigatório! Digite quantos lugares disponíveis o carro tem")
+	private int capacidade;
+
+	@OneToMany
+	@JsonIgnoreProperties("veiculo")
+	private Viagem viagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+
+	public Viagem getViagem() {
+		return viagem;
+	}
+
+	public void setViagem(Viagem viagem) {
+		this.viagem = viagem;
+	}
 
 }
