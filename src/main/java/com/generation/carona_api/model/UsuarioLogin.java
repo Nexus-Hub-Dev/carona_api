063ccc445d5c1b008a30301e7a936197ecab75ed
guitxc <guisandolidna@gmail.com>
2026-07-31 11:14:04 -0300

    :sparkles: feat: Usuario Login Finalizado!

--- src/main/java/com/generation/carona_api/model/UsuarioLogin.java
+++ src/main/java/com/generation/carona_api/model/UsuarioLogin.java
@@ -1,5 +1,56 @@
 package com.generation.carona_api.model;
 
 public class UsuarioLogin {
-
+	
+	private Long id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	private String celular;
+	private String foto;
+	private String token;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
 }
