b0c82d3f9e0ab1ffa1f8bd372e716d6102ebcb44
João Victor Ribeiro <joao.vsantos2@gmail.com>
2026-07-31 13:11:50 -0300

    feat: cria repository e controller de viagem integrados ao service

--- src/main/java/com/generation/carona_api/controller/ViagemController.java
+++ src/main/java/com/generation/carona_api/controller/ViagemController.java
@@ -1,5 +1,78 @@
 package com.generation.carona_api.controller;
 
+import com.generation.carona_api.model.Viagem;
+import com.generation.carona_api.repository.ViagemRepository;
+import com.generation.carona_api.service.ViagemService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/viagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ViagemController {
 
+    @Autowired
+    private ViagemRepository viagemRepository;
+
+    @Autowired
+    private ViagemService viagemService;
+
+    @GetMapping
+    public ResponseEntity<List<Viagem>> getAll() {
+        return ResponseEntity.ok(viagemRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Viagem> getById(@PathVariable Long id) {
+        return viagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/partida/{partida}")
+    public ResponseEntity<List<Viagem>> getByPartida(@PathVariable String partida) {
+        return ResponseEntity.ok(viagemRepository.findAllByPartidaContainingIgnoreCase(partida));
+    }
+
+    @GetMapping("/destino/{destino}")
+    public ResponseEntity<List<Viagem>> getByDestino(@PathVariable String destino) {
+        return ResponseEntity.ok(viagemRepository.findAllByDestinoContainingIgnoreCase(destino));
+    }
+
+    @PostMapping
+    public ResponseEntity<Viagem> post(@Valid @RequestBody Viagem viagem) {
+     
+        Viagem viagemCalculada = viagemService.calcularEntrega(viagem);
+        
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(viagemRepository.save(viagemCalculada));
+    }
+
+    @PutMapping
+    public ResponseEntity<Viagem> put(@Valid @RequestBody Viagem viagem) {
+        if (viagem.getId() == null || !viagemRepository.existsById(viagem.getId())) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // 
+        Viagem viagemCalculada = viagemService.calcularEntrega(viagem);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(viagemRepository.save(viagemCalculada));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        if (!viagemRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Viagem não encontrada.");
+        }
+        viagemRepository.deleteById(id);
+    }
 }
--- src/main/java/com/generation/carona_api/repository/ViagemRepository.java
+++ src/main/java/com/generation/carona_api/repository/ViagemRepository.java
@@ -1,5 +1,16 @@
 package com.generation.carona_api.repository;
 
-public interface ViagemRepository {
+import com.generation.carona_api.model.Viagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+
+    List<Viagem> findAllByPartidaContainingIgnoreCase(@Param("partida") String partida);
+
+    List<Viagem> findAllByDestinoContainingIgnoreCase(@Param("destino") String destino);
 }
